97171fa1c49ad5b55ec44a2ba1ab0f727d7da362
Maksym Dukov <maksym.dukov@litslink.com>
2021-10-02 17:38:39 +0300

    cleanup

--- load-test.sh
+++ load-test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo '=============RUNNIG TEST AGAINST NESTJS-GRAPHQL SERVER============='
+echo '200 requests with concurrency 1 to measure mean latency'
 loadtest -k -n 200 -c 1 -T application/json -p ./load-test/query.js http://localhost:3339/graphql
 echo '=============RUNNIG TEST AGAINST PURE APOLLO-GRAPHQL SERVER============='
+echo '200 requests with concurrency 1 to measure mean latency'
 loadtest -k -n 200 -c 1 -T application/json -p ./load-test/query.js http://localhost:3339/graphql-pure
